8c6f8a2ec36b6dd2744d483c782e34da21b76247
Max <magicmaxmark@gmail.com>
2019-10-01 11:20:17 -0400

    Fixed Movement Problems
    now uses addforce(x, impulse) to move the charecter leads to really zoomy movement

--- Source/Assets/Scripts/PlayerScript.cs
+++ Source/Assets/Scripts/PlayerScript.cs
@@ -14,21 +14,20 @@ public class PlayerScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
     }
 
     void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal");
-        if (Input.GetAxis("Jump") > 0)
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            RaycastHit2D hit = Physics2D.Raycast(rayOrigin.transform.position, Vector2.down, rayCheckDistance);
-            if (hit.collider != null)
-            {
-                rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-                anim.Play("Jump");
-            }
+            rb.AddForce(new Vector2(speed, 0), ForceMode2D.Impulse);
         }
-        rb.velocity = new Vector3(x * speed, rb.velocity.y, 0);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(new Vector2(-speed, 0), ForceMode2D.Impulse);
+        }
 
     }
 }
