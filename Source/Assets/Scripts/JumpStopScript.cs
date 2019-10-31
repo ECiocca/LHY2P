e35c9254c1539549cb0da677fb8367074ccf4db5
Max <magicmaxmark@gmail.com>
2019-10-31 11:51:56 -0400

    made feet collide (broken)

---
+++ Source/Assets/Scripts/JumpStopScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpStopScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(":)");
+        transform.parent.GetComponent<PlayerScript2>().isJumping = false;
+    }
+}
